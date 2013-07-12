702fafbc54ce2b50385bf55cfcdf22dee2e1d8b7
Mike Chevett <mchevett@gmail.com>
2013-07-11 22:35:19 -0400

    cool beans

---
+++ README.md
@@ -0,0 +1,14 @@
+snow
+======
+http://www.securelist.com/en/blog/208188101/Dangerous_whitespaces
+
+npm install
+
+echo 'console.log("wut?");' > my.js
+
+node app.js my.js
+
+less my.js.ws.go.min
+
+node my.js.ws.go.min
+
---
+++ app.js
@@ -0,0 +1,57 @@
+var fs = require('fs'), 
+	smusher = require('node-minify');
+
+
+function toWhiteSpace(v){
+	var ans = '',t;
+
+	for (var i=0; i<v.length; i++){
+
+		t = (v.charCodeAt(i).toString(2))
+		t = new Array(9-t.length).join('0')+t;
+		t = t.replace(/1/g, ' ').replace(/0/g, '	');
+		
+		ans +=t;
+	}
+
+	return ans;
+}
+
+function fromWhiteSpace(str){
+	var ans = '', code, bCode, wCode, chr;
+
+	for (var i=0; i<str.length; i+=8){
+		wCode = str.substr(i, 8);
+		bCode = wCode.replace(/ /g, '1').replace(/	/g, '0');
+		code = parseInt(bCode, 2);
+		chr = String.fromCharCode(code);
+
+		ans+=chr;
+	}
+
+	return ans;
+}
+
+var fileName = process.argv[2],
+	wsFileName = fileName+'.ws',
+	goFileName = fileName+'.ws.go',
+	minFileName = fileName+'.ws.go.min',sud
+	js = fs.readFileSync(fileName).toString(),
+	ws = toWhiteSpace(js),
+	go = fromWhiteSpace.toString().replace(/fromWhiteSpace/, 'xj5') + 'eval(xj5("'+ws+'"));';
+
+
+fs.writeFileSync(wsFileName, ws);
+
+
+fs.writeFileSync(goFileName, go);
+
+new smusher.minify({
+    type: 'yui-js',
+    fileIn: goFileName,
+    fileOut: minFileName,
+    callback: function(err){
+    	if (err)
+        	console.log(err);
+    }
+});
